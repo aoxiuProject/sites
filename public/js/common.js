b4d2a63a05e206998019b78d1f6cb09608f06d84
chuanzong.lcz <chuanzong.lcz@alibaba-inc.com>
2016-07-24 20:02:48 +0800

    tab to spaces

--- public/js/common.js
+++ public/js/common.js
@@ -1,31 +1,31 @@
-;(function ($, win, doc) {
-	var legend = win.legend = win.legend || {};
-	var html = legend.html = legend.html || {};
+;(function (win, doc) {
+  const legend = win.legend = win.legend || {};
+  const html = legend.html = legend.html || {};
 
-	html.loadScript = loadScript();
+  html.loadScript = loadScript();
 
-	function loadScript() {
-	  var otherScript = doc.getElementsByTagName('script')[0];
+  function loadScript() {
+    const otherScript = doc.getElementsByTagName('script')[0];
 
-	  return function (url, succ, cls) {
-	    //console.log('load url: ' + url);
-	    var script = doc.createElement("script");
-	    script.async = true;
+    return function (url, succ, cls) {
+      //console.log('load url: ' + url);
+      const script = doc.createElement("script");
+      script.async = true;
 
-	    if (!url) {
-	      return ;
-	    }
+      if (!url) {
+        return ;
+      }
 
-	    cls && (script.className = cls);
-	    
-	    if (succ) {
-	      script.onload = function() {              
-	        succ(script);
-	      }
-	    }
+      cls && (script.className = cls);
+      
+      if (succ) {
+        script.onload = function() {              
+          succ(script);
+        }
+      }
 
-	    script.src = url;
-	    otherScript.parentNode.appendChild(script);
-	  };
-	}
-}(jQuery, window, document));
+      script.src = url;
+      otherScript.parentNode.appendChild(script);
+    };
+  }
+}(window, document));
